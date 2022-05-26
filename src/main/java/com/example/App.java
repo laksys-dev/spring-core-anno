97a40d857eb1a9721deca794b6d1fc7f6eacebde
laksys <laksys@gmail.com>
2022-05-26 22:13:24 +0530

    initial commit

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("-----------main starts-----------");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+
+        Customer cust = ctx.getBean("cust", Customer.class);
+        System.out.println(cust);
+
+        for(String name: ctx.getBeanDefinitionNames())
+            System.out.println(name);
+        
+        ((ClassPathXmlApplicationContext)ctx).close();
+        System.out.println("------------main ends------------");
+    }
+}
---
+++ src/main/java/com/example/Customer.java
@@ -0,0 +1,29 @@
+package com.example;
+
+public class Customer {
+    private int id;
+    private String cname, address;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getCname() {
+        return cname;
+    }
+    @Override
+    public String toString() {
+        return "Customer [address=" + address + ", cname=" + cname + ", id=" + id + "]";
+    }
+    public void setCname(String cname) {
+        this.cname = cname;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+}
